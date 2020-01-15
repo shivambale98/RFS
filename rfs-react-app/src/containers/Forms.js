a72d5102663dc5084fa8b2400db595d1fe1aa854
shivam bale <b.shivam@novasemita.org>
2020-01-15 10:45:26 +0530

    first commit

---
+++ rfs-react-app/src/containers/Forms.js
@@ -0,0 +1,18 @@
+import React, { Componenet, Component } from "react";
+import Aux from '../hoc/Auxiliary';
+
+
+class Forms extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Forms</div>
+                <div>FormButtons</div>
+            </Aux>    
+
+        );
+    }
+}
+
+
+export default Forms;
